e0b094267fba6a32c25d89b3bfa0eca45eaf9ab3
Bence Kormos <ben.kormos@gmail.com>
2020-09-05 21:37:28 +0100

    update cells near the mines

--- src/grid.spec.ts
+++ src/grid.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { genereateCoord, generateMines, createGrid } from './grid';
+import { genereateCoord, updateCellsAroundMine, generateMines, createGrid } from './grid';
 
 describe('Grid', () => {
 
@@ -22,12 +22,117 @@ describe('Grid', () => {
 
   describe('createGrid', () => {
     
-    it('creates a grid of required dimensions', () => {
-      const grid1 = createGrid(10, 1);
-      expect(grid1).to.have.lengthOf(1);
-      expect(grid1[0]).to.have.lengthOf(10);
+    it('creates a grid of required dimensions filled with zeros', () => {
+      const grid = createGrid(5, 5);
+      expect(grid).to.eql([
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0]
+      ]);
     });
 
+  });
+
+  describe('updateCellsAroundMine', () => {
+    
+    it('updates cells around a mine', () => {
+      const grid = createGrid(5, 5);
+      grid[2][2] = 9;
+
+      console.log(grid);
+      updateCellsAroundMine(grid, 2, 2);
+      console.log(grid);
+
+      expect(grid).to.eql([
+        [0,0,0,0,0],
+        [0,1,1,1,0],
+        [0,1,9,1,0],
+        [0,1,1,1,0],
+        [0,0,0,0,0],
+      ]);
+    });
+
+    it('updates cells around two mines', () => {
+      const grid = createGrid(5, 5);
+      grid[1][1] = 9;
+      grid[3][3] = 9;
+
+      console.log(grid);
+      updateCellsAroundMine(grid, 1, 1);
+      updateCellsAroundMine(grid, 3, 3);
+      console.log(grid);
+
+      expect(grid).to.eql([
+        [1,1,1,0,0],
+        [1,9,1,0,0],
+        [1,1,2,1,1],
+        [0,0,1,9,1],
+        [0,0,1,1,1],
+      ]);
+    });
+
+    it('updates cells around four mines', () => {
+      const grid = createGrid(5, 5);
+      grid[1][1] = 9;
+      grid[1][3] = 9;
+      grid[3][1] = 9;
+      grid[3][3] = 9;
+
+      console.log(grid);
+      updateCellsAroundMine(grid, 1, 1);
+      updateCellsAroundMine(grid, 1, 3);
+      updateCellsAroundMine(grid, 3, 1);
+      updateCellsAroundMine(grid, 3, 3);
+      console.log(grid);
+
+      expect(grid).to.eql([
+        [1,1,2,1,1],
+        [1,9,2,9,1],
+        [2,2,4,2,2],
+        [1,9,2,9,1],
+        [1,1,2,1,1],
+      ]);
+    });
+
+    it('handles corner cells', () => {
+      const grid = createGrid(5, 5);
+      grid[0][0] = 9;
+      grid[0][4] = 9;
+      grid[4][0] = 9;
+      grid[4][4] = 9;
+
+      console.log(grid);
+      updateCellsAroundMine(grid, 0, 0);
+      updateCellsAroundMine(grid, 0, 4);
+      updateCellsAroundMine(grid, 4, 0);
+      updateCellsAroundMine(grid, 4, 4);
+      console.log(grid);
+
+      expect(grid).to.eql([
+        [9,1,0,1,9],
+        [1,1,0,1,1],
+        [0,0,0,0,0],
+        [1,1,0,1,1],
+        [9,1,0,1,9],
+      ]);
+    });
+
+  });
+
+  describe('generateMines', () => {
+    it('generates mines', () => {
+      const grid = createGrid(5, 5);
+      const mineCoords = generateMines(grid, 2, 5, 5);
+      
+      expect(mineCoords).to.have.lengthOf(2);
+  
+      const [ [y1, x1], [y2, x2] ] = mineCoords;
+      console.log(grid);
+      expect(grid[y1][x1], `[${y1}][${x1}]`).to.equal(9);
+      expect(grid[y2][x2], `[${y2}][${x2}]`).to.equal(9);
+    });
   });
 
 });
--- src/grid.ts
+++ src/grid.ts
@@ -7,22 +7,41 @@ export function genereateCoord(maxX: number, maxY: number): Coord {
   return [ x, y ];
 }
 
-export function generateMines(grid: Grid, mines: number, maxX: number, maxY: number): Grid {
-  const mineCoords: Record<string, boolean> = {};
-  let i: number = 0;
-  while (i < mines) {
-    const [ x, y ] = genereateCoord(maxX, maxY);
-    const cell: string = `${x}:${y}`;
-    if (!mineCoords[cell]) {
-      mineCoords[cell] = true;
-      grid[y][x] = 9;
-      i++;
+export function updateCellsAroundMine(grid: Grid, mineX: number, mineY: number): Grid {
+  let x: number = mineX - 1;
+  while (x <= mineX + 1) {
+    let y: number = mineY - 1;
+    while (y <= mineY + 1) {
+      const isMineCell: boolean = (x === mineX && y === mineY);
+      if (!isMineCell && grid[x] && !isNaN(grid[x][y])) {
+        grid[x][y] += 1;
+      }
+      y++;
     }
+    x++;
   }
   return grid;
 }
 
-export function createGrid(columns: number, rows: number): Grid {
+export function generateMines(grid: Grid, mines: number, maxX: number, maxY: number): Array<Coord> {
+  const mineCoordsIndex: Record<string, boolean> = {};
+  const coords: Array<Coord> = [];
+  let i: number = 0;
+  while (i < mines) {
+    const [ x, y ] = genereateCoord(maxX, maxY);
+    const cell: string = `${x}:${y}`;
+    if (!mineCoordsIndex[cell]) {
+      mineCoordsIndex[cell] = true;
+      // TODO refactor and flip x,y
+      coords.push([y, x]);
+      grid[y][x] = 9;
+      i++;
+    }
+  }
+  return coords;
+}
+
+export function createGrid(rows: number, columns: number): Grid {
   const grid: Grid = [];
   for (let i = 0; i < rows; i++) {
     const row: Array<number> = new Array(columns);
@@ -32,9 +51,11 @@ export function createGrid(columns: number, rows: number): Grid {
   return grid;
 }
 
-export function generateGrid(columns: number, rows: number, mines: number): Grid {
-  const grid: Grid = createGrid(columns, rows);
-  return generateMines(grid, mines, columns, rows);
+export function generateGrid(rows: number, columns: number, mines: number): Grid {
+  const grid: Grid = createGrid(rows, columns);
+  const mineCoords: Array<Coord> = generateMines(grid, mines, rows, columns);
+  mineCoords.forEach(([x, y]: Coord) => updateCellsAroundMine(grid, x, y));
+  return grid;
 }
 
 // TODO
